36488e3a5f4d5ac2ea7cf11ff5d71215598653af
d.kadyrbekov <d.kadyrbekov@dlg.im>
2019-09-15 21:36:59 +0300

    implementing TEST in Biz

--- hw7_microservice/service.go
+++ hw7_microservice/service.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"log"
 	"net"
 )
@@ -15,7 +19,9 @@ type ACLConfig struct {
 	ACLs map[string][]string `json:"-"`
 }
 
-type BizImplementation struct {}
+type BizImplementation struct {
+	ACL ACLConfig
+}
 
 func (b BizImplementation) Check(ctx context.Context, nothing *Nothing) (*Nothing, error) {
 	return &Nothing{}, nil
@@ -26,15 +32,25 @@ func (b BizImplementation) Add(ctx context.Context, nothing *Nothing) (*Nothing,
 }
 
 func (b BizImplementation) Test(ctx context.Context, nothing *Nothing) (*Nothing, error) {
+	fmt.Printf("test method is worked")
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		fmt.Printf("Unauthenticated bs no metadata")
+		err := status.Error(codes.Unauthenticated, "can't get metadata from context")
+		return nil, err //codes.Unauthenticated//errors.New("can't get metadata from context")
+	}
+	fmt.Printf("consumerVAlues = %v \n", md.Get("consumer"))
+
+	//_, ok := b.ACL[ctx.Value()]
 	return &Nothing{}, nil
 }
 
-func newBizImplementation() BizImplementation{
-	return BizImplementation{}
+func newBizImplementation(ACL ACLConfig) BizImplementation{
+	return BizImplementation{ACL}
 }
 
 type AdminServerImplementation struct {
-
+	ACL ACLConfig
 }
 
 func (a AdminServerImplementation) Logging(*Nothing, Admin_LoggingServer) error {
@@ -45,8 +61,8 @@ func (a AdminServerImplementation) Statistics(*StatInterval, Admin_StatisticsSer
 	return nil
 }
 
-func newAdminServer() AdminServerImplementation{
-	return AdminServerImplementation{}
+func newAdminServer(ACL ACLConfig) AdminServerImplementation{
+	return AdminServerImplementation{ACL}
 }
 
 func StartMyMicroservice(ctx context.Context, listenAddr string, ACLData string) error {
@@ -60,18 +76,17 @@ func StartMyMicroservice(ctx context.Context, listenAddr string, ACLData string)
 }
 
 func startMicroservise (ctx context.Context, listenAddr string, ACL ACLConfig) {
-	lis, err := net.Listen("tcp", ":8081")
+	lis, err := net.Listen("tcp", listenAddr)
 	if err != nil {
-		log.Fatalln("cant listet port:", err)
+		log.Fatalln("cant listen port:", err)
 	}
 	defer lis.Close()
 
 	server := grpc.NewServer()
 
-	RegisterAdminServer(server, newAdminServer())
-	RegisterBizServer(server, newBizImplementation())
+	RegisterAdminServer(server, newAdminServer(ACL))
+	RegisterBizServer(server, newBizImplementation(ACL))
 
 	go server.Serve(lis)
-
 	<-ctx.Done()
 }
